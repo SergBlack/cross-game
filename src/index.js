5ce04158a472afaa8d298701beb408cc2cd59515
Vertigo <vertigomaill@gmail.com>
2019-12-09 18:24:34 +0300

    2nd commit

--- src/index.js
+++ src/index.js
@@ -4,21 +4,15 @@ import "./index.css";
 import App from "./App";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          this.setState({ value: "X" });
+          this.props.onClick();
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -29,8 +23,18 @@ class Board extends React.Component {
     super(props);
     this.state = { squares: Array(9).fill(null) };
   }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
